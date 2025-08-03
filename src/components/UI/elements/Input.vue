d3ac00c2a7e4cf9fb39127d3b58456b1d51b7324
GeorgeTheScripter <albegoff.georgy@yandex.ru>
2025-08-03 08:00:28 +0300

    add counter to inputs

--- src/components/UI/elements/Input.vue
+++ src/components/UI/elements/Input.vue
@@ -1,18 +1,26 @@
 <template>
-  <input
-    class="py-4 px-4 bg-gray-300 rounded-xl outline-0"
-    type="text"
-    :value="modelValue"
-    @input="handleInput($event)"
-  />
+  <div class="w-full relative">
+    <input
+      class="py-4 px-4 bg-gray-300 rounded-xl outline-0 w-full"
+      type="text"
+      :value="modelValue"
+      @input="handleInput($event)"
+    />
+
+    <div class="absolute top-1/2 -translate-y-1/2 right-4 text-gray-400">
+      {{ charCount }}
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
+import { computed, ref, watch } from "vue";
+
 defineOptions({
   name: "Input",
 });
 
-defineProps<{ modelValue: string | number | null }>();
+const props = defineProps<{ modelValue: string | number | null }>();
 
 const emit = defineEmits<{
   "update:modelValue": [value: string | number | null];
@@ -21,4 +29,9 @@ const emit = defineEmits<{
 const handleInput = (event: Event) => {
   emit("update:modelValue", (event.target as HTMLInputElement).value);
 };
+
+const charCount = computed(() => {
+  if (props.modelValue === null) return 0;
+  return String(props.modelValue).length;
+});
 </script>
